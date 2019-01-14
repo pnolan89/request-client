d76e19f028314cb205b5834ce1173f3bc9cdefd9
pnolan89 <petenol@gmail.com>
2019-01-14 21:59:41 +0000

    added request function

---
+++ request.js
@@ -0,0 +1,17 @@
+var request = require('request');
+var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')
+  .on('error', function(err) {
+    throw err;
+  })
+  .on('response', function (response) {
+    console.log('Downloading image...');
+    console.log('Response Status Code: ' + response.statusCode);
+    console.log('Response Message: ' + response.statusMessage);
+    console.log('Content Type: ' + response.headers['content-type']);
+  })
+  .pipe(fs.createWriteStream('./future.jpg')
+  .on('finish', function() {
+    console.log('Download Complete!');
+  }));
